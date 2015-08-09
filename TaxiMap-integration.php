d98d9ef98d07dd5bfd92e5eb20c013df5e505f30
nimusdigital <nimusdigital@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-09 12:14:25 +0000

    Adding first version of TaxiMap Integration plugin to Wordpress repo
    git-svn-id: https://plugins.svn.wordpress.org/taximap-integration/trunk@1216296 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ TaxiMap-integration.php
@@ -0,0 +1,136 @@
+<?php
+/*
+Plugin Name: TaxiMap Integration
+Plugin URI: http://blog.taximap.co.uk/2015/08/wordpress-plugin/
+Description: Displays the TaxiMap taxi fare price calculator on your site via shortcode [taximap] or widget.
+Version: 1.0
+Author: M Williams
+Author URI: http://nimbus.agency
+*/
+
+
+function taximap(){
+	ob_start();
+	return tm_renderiFrame();
+}
+
+
+
+function tm_scripts(){
+	wp_enqueue_style( 'style-name', plugins_url( 'taximap.css', __FILE__ ) );
+	wp_enqueue_script( 'script-name', plugins_url( 'taximap.js', __FILE__ ) );
+}
+
+
+function tm_renderiFrame(){
+	$taximapId=esc_attr( get_option('taximap_id') );
+	$tmIframe="";
+	if($taximapId==''){
+		$taximapId='10000';	//default to TaxiMap Demo Account
+		$tmIframe.='<div class="tm_alert">Warning: TaxiMap ID not set - Admin must add a TaxiMap ID. </div>';
+	}
+	$tmIframe.='<div class="taximap"><!-- Version 1508041748 -->';
+	$tmIframe.='<iframe src="//taximap.co.uk/plugin/taxi_map_frame.asp?wp=shortcodeWP&i1=1&f=1&uid='.$taximapId.'"></iframe>';
+	$tmIframe.='<!-- Powered by TaxiMap.co.uk -->';
+	$tmIframe.='</div>';
+	return $tmIframe;
+}
+
+add_action('wp_enqueue_scripts','tm_scripts');
+
+add_shortcode('taximap', 'taximap');
+
+// create custom plugin settings menu
+add_action('admin_menu', 'taximap_integration_create_menu');
+
+function taximap_integration_create_menu() {
+
+	//create new top-level menu
+	add_menu_page('TaxiMap Integration', 'TaxiMap Settings', 'administrator', __FILE__, 'taximap_integration_settings_page','dashicons-location' );
+
+	//call register settings function
+	add_action( 'admin_init', 'taximap_integration_settings' );
+}
+
+
+function taximap_integration_settings() {
+	//register our settings
+	register_setting( 'TaxiMap-integration-settings-group', 'taximap_id' );
+}
+
+function taximap_integration_settings_page() {
+?>
+<div class="wrap">
+<h2>TaxiMap Integration</h2>
+<p>You will need to get your TaxiMap Membership No. from <a href="https://taximap.co.uk/members/" target="_blank">http://taximap.co.uk</a> (found as the top item in the STATUS table on the right side after loggin in) and enter it below...<br />All other configuration is done from within your <a href="https://taximap.co.uk/members/" target="_blank">TaxiMap account</a>.</p>
+	<form method="post" action="options.php"> 
+		
+		<?php settings_fields( 'TaxiMap-integration-settings-group' ); ?>
+		<?php do_settings_sections( 'TaxiMap-integration-settings-group' ); ?>
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row">TaxiMap User ID (Membership No):</th>
+					<td><input type="text" name="taximap_id" value="<?php echo esc_attr(get_option('taximap_id')); ?>" /></td>
+				</tr>
+			</table>
+		
+	<?php submit_button(); ?>
+	</form>
+</div>
+<?php }
+
+
+//**** TAXIMAP WIDGET *****\\
+
+class TaxiMapWidget extends WP_Widget
+{
+  function TaxiMapWidget()
+  {
+    $widget_ops = array('classname' => 'TaxiMapWidget', 'description' => 'Displays TaxiMap fare price calculator in your widebar (or widget area)' );
+    $this->WP_Widget('TaxiMapWidget', 'TaxiMap', $widget_ops);
+  }
+ 
+  function form($instance)
+  {
+    $instance = wp_parse_args( (array) $instance, array( 'title' => '', 'height' => '320', 'taximapId' => esc_attr(get_option('taximap_id')) ) );
+    $title = $instance['title'];
+	$taximapIdForWidget = $instance['taximapId'];
+	$height = $instance['height'];
+?>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+  <p><label for="<?php echo $this->get_field_id('taximapId'); ?>">TaxiMap Membership No: <input class="widefat" id="<?php echo $this->get_field_id('taximapId'); ?>" name="<?php echo $this->get_field_name('taximapId'); ?>" type="text" value="<?php echo attribute_escape($taximapIdForWidget); ?>" /></label></p>
+  <p><label for="<?php echo $this->get_field_id('height'); ?>">Height (pixels): <input class="widefat" id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" type="text" value="<?php echo attribute_escape($height); ?>" /></label></p>
+<?php
+  }
+ 
+  function update($new_instance, $old_instance)
+  {
+    $instance = $old_instance;
+    $instance['title'] = $new_instance['title'];
+	$instance['taximapId'] = $new_instance['taximapId'];
+	$instance['height'] = $new_instance['height'];
+    return $instance;
+  }
+ 
+  function widget($args, $instance)
+  {
+    extract($args, EXTR_SKIP);
+ 
+    echo $before_widget;
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+	$height = $instance['height'];
+	$taximapId = $instance['taximapId'];
+ 
+    if (!empty($title))
+      echo $before_title . $title . $after_title;
+ 
+    // WIDGET CODE GOES HERE
+    //echo "<h1>This is my new widget!</h1>";
+	echo '<div class="tm_widget"><iframe style="height:'.$height.'px;" src="//taximap.co.uk/plugin/taxi_map_frame.asp?wp=widget&i1=1&f=1&uid='.$taximapId.'"></iframe></div>';
+ 
+    echo $after_widget;
+  }
+ 
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("TaxiMapWidget");') );
+?>
---
+++ readme.txt
@@ -0,0 +1,79 @@
+=== TaxiMap Integration ===
+Contributors: nimbusdigital
+Tags: taxi,map,taximap,cab,minicab,mini-cab,fare,price,calculator,journey,travel,transport,uber,estimate,calculate,booking,system
+Requires at least: 3.0
+Tested up to: 4.2.3
+Stable tag: trunk
+License: GP2
+
+Displays the TaxiMap fare price calculator on your site via shortcode [taximap] or widget.
+
+== Description ==
+This Wordpress plugin adds the TaxiMap taxi fare price calculator to your Wordpress site. The TaxiMap calculator can be displayed as part of the main content of a page or post by using the shortcode: [taximap] within your post, or it can be added as a widget to your sidebar (or other widget area within your Wordpress template).
+
+TaxiMap is a highly configurable journey price calculator based on a price per mile. It uses Google Maps to plot and display routes. It offers many further configuration options, inlcuding:
+*	Booking alerts via SMS text message to your mobile device (primarily UK)
+*	Credit card payment processing via multiple providers such as Stripe, Paypal & SagePay
+*	Multiple vehicle pricing
+*	Meet & Greet pricing for airport transfers
+*	Fixed pricing for specific journeys (zone to zone pricing)
+*	Time based pricing varation (hours of the day or day of year)
+*	Wait & return pricing
+*	Posts to your own booking form (option)
+
+
+
+== Requirements ==
+An active TaxiMap account: https://taximap.co.uk
+
+
+
+== Installation ==
+Copy the plugin folder to your Wordpress plugins directory - typically wp-install-directory/wp-content/plugins/
+
+Activate the plugin:
+Log in to the Wordpress admin, go to Plugins > Installed Plugins.
+Find the TaxiMap-Integration item and click Activate
+
+Configure your TaxiMap ID:
+In the admin menu, click TaxiMap Settings
+Enter your TaxiMap.co.uk membership number. You can find this under STATUS on your TaxiMap Profile page, after logging in to https://taximap.co.uk
+Save
+
+Add shortcode to a page:
+Create (or edit) a page (or post) on your Wordpress site where you want your TaxiMap Price Calculator to appear.
+Enter the shortcode: [taximap] at the point on the page where you want the calculator to be displayed.
+
+By default, the TaxiMap calculator will take 100% width and 500 pixels in height.
+You might want to consider making the page/post a full-width page (no sidebar).
+You can modify the proportions by either modifying the taximap.css file (using the plugin editor) or by adding your own CSS properties for the class: .taximap iframe
+
+
+Add a widget to your sidebar:
+From the Wordpress admin page menu, select Widgets from the Appearance section.
+Under 'Available Widgets' look for the item named TaxiMap and drag it to you preferred widget area (on the right)
+Expand the new widget and enter a title (abitrary text of your discerning), your TaxiMap Membership no (as described above), and the height you wish the widget to have on your sidebar/widget area.
+
+For more details and screenshots, see http://blog.taximap.co.uk/2015/08/wordpress-plugin/
+
+== Frequently Asked Questions ==
+
+= Where do I configure jouney price calculation settings? =
+
+All price calculations settings are configured from within your TaxiMap account.
+
+= Where can I get installation help? =
+
+http://blog.taximap.co.uk/2015/08/wordpress-plugin/
+
+
+
+== Screenshots ==
+1. Initial TaxiMap screen where journey points are entered (1-taximap-integration-initial-screen.gif)
+2. Estimated prices shown (2-taximap-integration-show-price-screen)
+3. Booking screen (3-taximap-integration-booking-screen)
+
+
+
+== Changelog ==
+1.0 Initial version.
